b273d537e67bf54c00be0a705e6cc0d6fbad2892
anupkelkar <anup.kelkar@trilogy.com>
2026-06-26 13:16:19 +0000

    Refactor the code

---
+++ ASCIIArt/figures/cow.py
@@ -0,0 +1,7 @@
+def draw_cow():
+    print("         (__)")
+    print("         (oo)")
+    print("  /-------\\/ ")
+    print(" / |     ||  ")
+    print("*  ||----||  ")
+    print("   ^^    ^^  ")
